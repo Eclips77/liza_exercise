ff97670c4453766ff8a8574f53183f627f296c31
Eclipse <Eclips77@users.noreply.github.com>
2025-05-18 19:45:26 +0300

    starting

--- SignalAnalyze/SignalAnalyze/Program.cs
+++ SignalAnalyze/SignalAnalyze/Program.cs
@@ -10,7 +10,7 @@ namespace SignalAnalyze
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("hello world!");
         }
     }
 }
